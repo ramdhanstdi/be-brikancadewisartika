af1f7ca2fa54b8452a79b841aa2123f39a57b1f1
ramdhanstdi <ramdhansetiadhi@gmail.com>
2023-05-06 08:55:21 +0700

    token fixing

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -7,7 +7,9 @@ const successResponse = require("../helpers/successResponse");
 const authorization = (req, res, next) => {
   console.log(req.headers.authorization);
   if (req.headers.authorization) {
-    const auth = req.headers.authorization.split(" ")[1];
+    const tokenHeaders = req.headers.authorization.split(" ")[1];
+    const token = JSON.parse(tokenHeaders);
+    const auth = token.token;
     console.log("split", auth);
     try {
       const result = jwt.verify(auth, process.env.APP_KEY || "k4Aul4h");
